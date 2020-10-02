9ab4b5551820d7e4a9d7e7d955701c7e8c2d9ae6
bnlhv <benlahav1990@gmail.com>
2020-10-02 11:50:25 +0300

    Updates

---
+++ precache-manifest.27160a9f78c74b41bd88dbb1e386d45d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dff6e073304d35ed575182ab0e33ec64",
+    "url": "/Weather-app-react/index.html"
+  },
+  {
+    "revision": "c110ef7735b74357e42a",
+    "url": "/Weather-app-react/static/css/2.e2eb1707.chunk.css"
+  },
+  {
+    "revision": "7ec6620bbfb85d6810e5",
+    "url": "/Weather-app-react/static/css/main.ae2224d7.chunk.css"
+  },
+  {
+    "revision": "c110ef7735b74357e42a",
+    "url": "/Weather-app-react/static/js/2.2876630b.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Weather-app-react/static/js/2.2876630b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ec6620bbfb85d6810e5",
+    "url": "/Weather-app-react/static/js/main.59b22f16.chunk.js"
+  },
+  {
+    "revision": "d2e1dce8f8541a3f82c4",
+    "url": "/Weather-app-react/static/js/runtime-main.c01284f1.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/Weather-app-react/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/Weather-app-react/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/Weather-app-react/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/Weather-app-react/static/media/slick.f97e3bbf.svg"
+  }
+]);
